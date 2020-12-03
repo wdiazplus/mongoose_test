d69ea5c301c27a0f65eda1a8fff0f7fcd23efbe9
wdiazplus <dev@wilmerdiaz.com>
2020-12-03 16:41:20 -0500

    Connected DB

---
+++ db/db.js
@@ -0,0 +1,20 @@
+const { config } = require('../config');
+const mongoose = require('mongoose');
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const DB_NAME = config.dbName;
+const DB_HOST = config.dbHost
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+
+mongoose.connect(
+  `mongodb+srv://${USER}:${PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`,
+  () => {
+    if (!mongoose.connect) {
+      console.log('Problem with connection to DB');
+    }
+    console.log('Database connected');
+  }
+);
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express')
+const { config } = require('./config');
+
+
 const app = express()
 
-const port = 8080
+const port = config.port
 
 
 app.get("/", (req, res)=>{
     res.send('Hi Postman')
 })
+
+
 
 app.listen(`${port}`,()=>{
     console.log(`Server connected in http://localhost:${port}`)
