d342758ba8be982d0c5bf43b579c64a9664ea6ee
wdiazplus <dev@wilmerdiaz.com>
2020-12-03 22:38:38 -0500

    Add endpoint get/post example

--- server.js
+++ server.js
@@ -1,18 +1,57 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
 const { config } = require('./config');
+require('./db/db');
+require('./models/courses');
+require('./models/movies');
+
+const app = express();
+const port = config.port;
+
+app.get('/movie', (req, res) => {
+  let Movie = mongoose.model('Movie');
+  Movie.find()
+    .then((doc) => {
+      res.json(doc);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
+});
+
+app.post('/movie', (req, res) => {
+  let Movie = mongoose.model('Movie');
+  Movie.create({
+    title:'Mi rpimer movie',
+    description:'rararararararararararararararararararararararararararararararararararara',
+    Rating:4,
+  })
+    .then((doc) => {
+      res.json(doc);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
+});
 
 
-const app = express()
 
-const port = config.port
+app.get('/course', (req, res) => {
+  let Course = mongoose.model('Course');
+  Course.find()
+    .then((doc) => {
+      res.json(doc);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
+});
 
 
-app.get("/", (req, res)=>{
-    res.send('Hi Postman')
-})
 
-
-
-app.listen(`${port}`,()=>{
-    console.log(`Server connected in http://localhost:${port}`)
-})
+app.listen(`${port}`, () => {
+  console.log(`Server connected in http://localhost:${port}`);
+});
